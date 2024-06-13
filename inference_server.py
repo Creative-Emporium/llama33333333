9d228864f837bd1ca654edd9af5f0f031d7fdbf5
k.l.lambda <k.l.lambda@gmail.com>
2024-06-13 16:31:04 +0800

    inference_server.py: handler of chat-completions implemented.

--- inference_server.py
+++ inference_server.py
@@ -1,7 +1,6 @@
 
-from typing import List, Optional
 import sys
-import torch
+import os
 import flask
 import fire
 import json
@@ -12,26 +11,55 @@ from llama import Dialog, Llama
 
 app = flask.Flask(__name__)
 
+os.environ['RANK'] = '0'
+os.environ['WORLD_SIZE'] = '1'
+os.environ['MASTER_ADDR'] = 'localhost'
+os.environ['MASTER_PORT'] = '12355'
+
 
 @app.route('/chat-completions', methods=['POST'])
 def chat ():
 	request = flask.request
-	print('request:', request.json)
+	#print('request:', request.json)
+
+	dialogs = [request.json['messages'][-3:]]
+
+	global g_generator, g_temperature, g_top_p
+	results = g_generator.chat_completion(
+		dialogs,
+		max_gen_len=2400,
+		temperature=g_temperature,
+		top_p=g_top_p,
+	)
 
 	return flask.Response(json.dumps({
-		'choices': [{
-			'message': {
-				'role': 'assistant',
-				'content': 'emm',
-			},
-		}],
+		'choices': [
+			dict(message=result['generation'])
+			for result in results
+		],
 	}), mimetype='application/json')
 
 
 def main (
+		ckpt_dir: str,
+		tokenizer_path: str,
 		port: int = 8080,
 		host: str = '127.0.0.1',
+		temperature: float = 0.6,
+		top_p: float = 0.9,
 	):
+	global g_generator, g_temperature, g_top_p
+
+	g_temperature = temperature
+	g_top_p = top_p
+
+	g_generator = Llama.build(
+		ckpt_dir=ckpt_dir,
+		tokenizer_path=tokenizer_path,
+		max_seq_len=3072,
+		max_batch_size=1,
+	)
+
 	try:
 		app.run(port=port, host=host, threaded=False)
 	except:
